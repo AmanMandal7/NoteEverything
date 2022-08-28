c83bf382ed99b70fb9e0476815d8c304ea6527f1
AmanGH009 <aman.mandal.71334@gmail.com>
2022-08-28 14:22:35 +0530

    Completed with  Authorization

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
 
 const app = express();
+connectDB();
+app.use(express.json());
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-    res.send("API is running...")
-})
+// app.get("/", (req, res) => {
+//     res.send("API is running...")
+// })
 
-app.get("/api/notes", (req, res) => {
-    res.send(notes)
-})
+// app.get("/api/notes", (req, res) => {
+//     res.send(notes)
+// })
 
-app.get("/api/notes/:id", (req, res) => {
-    const note = notes.find((n) => n._id === req.params.id);
+app.use('/api/users', userRoutes)
+app.use(notFound);
+app.use(errorHandler);
 
-    res.send(note);
-})
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import LandingPage from './screens/LandingPage/LandingPage';
+import MyNotes from './screens/MyNotes/MyNotes';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen/RegisterScreen';
 
-function App() {
-  return (
-    <>
-      <div className="App">
-        Hi, welcome to noteeverything
-      </div>
-    </>
-  );
-}
+
+const App = () => (
+  <BrowserRouter>
+    <Header />
+    <main>
+      <Routes>
+        <Route exact path='/' element={<LandingPage />} />
+        <Route exact path='/login' element={<LoginScreen />} />
+        <Route exact path='/register' element={<RegisterScreen />} />
+        <Route exact path='/mynotes' element={<MyNotes />} />
+      </Routes>
+    </main>
+    <Footer />
+  </BrowserRouter>
+);
+
 
 export default App;
